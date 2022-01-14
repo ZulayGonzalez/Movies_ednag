d5c3c564026e42b50759fb3d5c9d6393a27a1071
ZulayGonzalez <49498847+ZulayGonzalez@users.noreply.github.com>
2022-01-14 17:19:14 -0500

    Add files via upload

--- movies_ednag/src/index.js
+++ movies_ednag/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import CardPage from './Pages/CardPage'
+import InicialPage from './Pages/InicialPage'
+import Card from './Components/Card';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardPage />
+    <InicialPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
